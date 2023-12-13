6350be0af18a802ed0589c223d3d0796e3e12ccc
Gamer <markhlibovytskyy@gmail.com>
2023-12-13 12:36:49 +0200

    added methods that are not present in the base class. Works as expected.

--- OOP_assignment_3/OOP_assignment_3.cpp
+++ OOP_assignment_3/OOP_assignment_3.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+#include <cmath>
+
 class Figure {
 public:
     virtual int getArea() = 0;
+    virtual void calculateDimensions(int area) = 0;
     virtual ~Figure() {}
 };
 
@@ -14,6 +18,13 @@ public:
 
     int getArea() override {
         return param1 * param2;
+    }
+
+    void calculateDimensions(int area) override {
+
+        int side = static_cast<int>(std::sqrt(area));
+
+        setSize(side, side);
     }
 
 private:
@@ -32,24 +43,45 @@ public:
         return 3.14 * rad * rad;
     }
 
+    void calculateDimensions(int area) override {
+
+        int radius = static_cast<int>(std::sqrt(area / 3.14));
+        setRadius(radius);
+    }
+
 private:
     int rad = 0;
 };
 
 int main() {
-    Figure* f = new Circle();
+    Figure* f1 = new Circle();
+    Figure* f2 = new Square();
 
-    Circle* c = dynamic_cast<Circle*>(f);
-    Square* s = dynamic_cast<Square*>(f);
+    Circle* c = dynamic_cast<Circle*>(f1);
+    Square* s = dynamic_cast<Square*>(f2);
 
-    c->setRadius(5);
-    s->setSize(10, 10);
+    if (c != nullptr) {
+        c->setRadius(5);
+        std::cout << "Area of the circle: " << c->getArea() << '\n';
 
-    std::cout << "Area of the circle" << c->getArea() << '\n';
-    std::cout << "Are of the square" << s->getArea() << '\n';
+        f1->calculateDimensions(50);
+        std::cout << "New area of the circle: " << c->getArea() << '\n';
+    }
 
-    delete f;
-    delete c;
+    if (s != nullptr) {
+        s->setSize(10, 10);
+        std::cout << "Area of the square: " << s->getArea() << '\n';
+
+        f2->calculateDimensions(200);
+        std::cout << "New area of the circle: " << s->getArea() << '\n';
+    }
+
+    if (c == nullptr && s == nullptr) {
+        std::cout << "The object is neither a Circle nor a Square." << '\n';
+    }
+
+    delete f1;
+    delete f2;
 
     return 0;
 }
